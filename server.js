cfccf709ffdabbff69c2174058405aadcd361e81
kenji <dgff2079@gmail.com>
2017-08-20 22:48:57 +0900

    console.logeventを記述

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.post('/webhook/', function (req, res) {
                 sendGenericMessage(sender)
                 continue
             }
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            console.log(event);
+            sendTextMessage(sender, text.substring(0, 200))
         }
         if (event.postback) {
             let text = JSON.stringify(event.postback)
