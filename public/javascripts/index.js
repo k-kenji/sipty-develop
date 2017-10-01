65cd19280714124a1d0a45cd387c687d78b54f31
kenji <dgff2079@gmail.com>
2017-10-01 19:32:03 +0900

    js cssをレスポンシブように変更、widthにおおじてメニューバーのdisplayoneの実装

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,18 @@
 (function($) {
     $(function() {
-        var $header = $('.header');
-        // Nav Toggle Button
-        $('#nav-toggle').click(function(){
-            $header.toggleClass('open');
-        });
+        $(window).resize(function(){
+            //windowの幅をxに代入
+            var w = $(window).width();
+            if(w <= 640) {
+                $(".header").addClass("none");
+                $("#top-head").removeClass("none");
+            }
+            });
+            
+        // var $header = $('.header');
+        // // Nav Toggle Button
+        // $('#nav-toggle').click(function(){
+        //     $header.toggleClass('open');
+        // });
     });
 })(jQuery);
