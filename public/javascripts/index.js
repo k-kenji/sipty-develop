feeb261e692424d1bfe449e71d7fb212065edd95
kenji <dgff2079@gmail.com>
2017-10-01 20:03:07 +0900

    jsをwidthによってクラスの付与を変更

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,12 +4,9 @@
             //windowの幅をxに代入
             var w = $(window).width();
             // width:640以下であればメニューをdisplaynoneに設定
-            // if(w <= 640) {
-            //     $(".header").addClass("none");
-
-            // } else {
-            //     $(".header").removeClass("none");
-            // }
+            if(w <= 640) {
+                $(".top-head").removeClass("mobile-none");
+            }
             });
     });
 })(jQuery);
