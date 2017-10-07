bd323814e4e225e705acc9c33abe99cd6488cfa2
kenji <dgff2079@gmail.com>
2017-10-07 18:23:50 +0900

    widthによってクラスをremoveするjs処理を無効化

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,17 +7,17 @@
         });
     });
 
-    $(window).resize(function() {
-        var w = $(window).width();
-        if(w <= 640) {
-            $("#container").removeClass("container");
-            $("#left-container").removeClass("left-container");
-            $("#description").removeClass("description");
-            // $("div").toggleClass("");
-        } else if(w >= 640) {
-            $("#container").addClass("container");
-            $("#left-container").addClass("left-container");
-            $("#description").addClass("description");
-        }
-    });
+    // $(window).resize(function() {
+    //     var w = $(window).width();
+    //     if(w <= 640) {
+    //         $("#container").removeClass("container");
+    //         $("#left-container").removeClass("left-container");
+    //         $("#description").removeClass("description");
+    //         // $("div").toggleClass("");
+    //     } else if(w >= 640) {
+    //         $("#container").addClass("container");
+    //         $("#left-container").addClass("left-container");
+    //         $("#description").addClass("description");
+    //     }
+    // });
 })(jQuery);
