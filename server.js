896b277feadebf7e3f46a452bf2b5c611bfd5165
kenji <dgff2079@gmail.com>
2017-11-02 21:51:16 +0900

    envファイルのアクセストークンを文字列で保存

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ app.post('/webhook/', function (req, res) {
               firstLoginMessage(sender); // この関数のあとでpromiseを実行
               console.log("psotbackまで到達");
               console.log(token);
+              console.log(ENV.FACEBOOK_TOKEN);
             } else if (event.postback.payload === "help") {
               startSipty(sender);
             } else if (event.postback.payload === "usesipty") {
