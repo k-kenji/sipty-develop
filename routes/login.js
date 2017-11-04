1834ec4749c56a134b34b6885a6b540dcdcbd119
kenji <dgff2079@gmail.com>
2017-11-04 16:07:07 +0900

    router loginファイルのpathを/に修正

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', function(request, response) {
+router.get('/', function(request, response) {
   response.render('login', { user: req.user });
 });
 
