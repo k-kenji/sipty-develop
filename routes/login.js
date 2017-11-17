798f73263f126ca3ad51e12b8bf009cdf32d13e0
kenji <dgff2079@gmail.com>
2017-11-17 21:32:58 +0900

    パラメータを渡せるかチェック

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', function(req, res) {
-  res.render('login');
+  res.render('login', {title:"login test"});
 });
 
 module.exports = router;
