d13e078f1278ea51fd14e9c77bd589c433dac4d0
kenji <dgff2079@gmail.com>
2017-11-17 21:45:59 +0900

    パラメーターにueridを渡してejsで表示する処理をッキ術

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', function(req, res) {
-  res.render('login', {title:"login test"});
+  res.render('login', {user: req.user.id});
 });
 
 module.exports = router;
