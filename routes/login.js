b289779e3457864477b0a149e4f610e736edcc00
kenji <dgff2079@gmail.com>
2017-11-18 15:29:40 +0900

    パラメーターにueridを渡してejsで表示する処理記述、エラーの場合にejsに条件分岐を記述

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', function(req, res) {
-  res.render('login', {user: req.user.email});
+  console.log(req);
+  console.log(res);
+  res.render('login', {user: req});
+
 });
 
 module.exports = router;
