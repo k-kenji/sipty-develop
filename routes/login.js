13bba81e6a4c9595fa372725ba161757a43e5b9d
kenji <dgff2079@gmail.com>
2017-12-20 23:43:42 +0900

    コメントアウト

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', function(req, res) {
-  console.log(req);
-  // console.log(res);
+  // console.log(req);
+  console.log(res);
   res.render('login', {user: req});
 
 });
