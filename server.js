777bc91667ed9e681f786f825dd687e6a4a97ff0
kenji <dgff2079@gmail.com>
2017-12-22 23:16:10 +0900

    herokuよりmLABでmongoni

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ const passport = require('passport'); // passportモジュール
 const ig = require('instagram-node').instagram();
 var accesstoken;
 
-// mongoDB接続
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const assert = require('assert')
 
-MongoClient.connect('mongodb://127.0.0.1:27017/testmydb', (err, db) => {
-    assert.equal(null, err)
-    console.log("Connected successfully to server")
-    db.close()
-})
 
 
 // instagram
