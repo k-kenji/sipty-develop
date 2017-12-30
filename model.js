f8649c5df7539f0b9934c1804603c527ccdf27b1
kenji <dgff2079@gmail.com>
2017-12-30 11:17:26 +0900

    models.js作成、mongoに接続処理記述

---
+++ model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost/firstapp');
+
+function validator(v) {
+  return v.length > 0;
+}
+
+var Post = new mongoose.Schema({
+    text   : { type: String, validate: [validator, "Empty Error"] }
+  , created: { type: Date, default: Date.now }
+});
+
+exports.Post = db.model('Post', Post);
