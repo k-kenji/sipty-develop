40f36ed1c35009dbfd4787c6dd555e3dbb94233e
kenji <dgff2079@gmail.com>
2018-01-05 21:51:25 +0900

    配列連結にjoinメソッドを利用

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.get('/handleAuth', function(req, res){
         var get_token = result.access_token; // ユーザーidを変数に格納
         var user_id_list = [];
         user_id_list =  get_token.split(".") // カンマでユーザーidを分割
-        var user_id = user_id_list.join();
+        var user_id = user_id_list.join('');
         console.log("連結した結果" + user_id);
         accesstoken = result.access_token; // アクセストークンをDBに保存する
     // After getting the access_token redirect to the '/' route 
